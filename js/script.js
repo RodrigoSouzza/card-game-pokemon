abd16015e8235ecbf4057909be90b6cac0ad6403
RodrigoSouzza <contato.rodrigoom@gmail.com>
2023-07-06 23:01:29 -0300

    funcoes criaCarta e criaListaCartas funcionando, cartas sendo geradas automaticamente

--- js/script.js
+++ js/script.js
@@ -22,23 +22,66 @@ function gerarVida(){
     return vida
 }
 
-async function criaCarta(pokemon){
+async function criaCarta(pokemon, carta){
 
     const data = await pokeApi(pokemon)
+
+    const nomePokemon = carta.querySelector('.nome')
+    const idPokemon = carta.querySelector('.numero')
+    const tipoPokemon = carta.querySelector('.tipo')
+    const ataquePokemon = carta.querySelector('.ataque')
+    const vidaPokemon = carta.querySelector('.vida')
+    const imagemPokemon = carta.querySelector('.imagem')
 
     nomePokemon.innerHTML = data.name
     idPokemon.innerHTML = data.id
     tipoPokemon.innerHTML = `Type: ${data.types[0].type.name}`                        
     ataquePokemon.innerHTML = `Attack: ${gerarAtaque()}`
     vidaPokemon.innerHTML = `Life: ${gerarVida()}`    
-    imagemPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];    
+    imagemPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']    
 }
 
-function numeroAleatorio(){
-    return Math.floor(Math.random() * 247) +1
+function criaListaCartas() {
+    const listaCartas = document.getElementById('lista_pokemons')
+  
+    for (let indice = 1; indice < 9; indice++) {
+      const carta = document.createElement('div')
+      carta.className = 'carta'
+      carta.id = `carta_${indice}`
+  
+      const imagem = document.createElement('div')
+      imagem.className = 'carta_imagem'
+      const img = document.createElement('img')
+      img.className = 'imagem'
+      img.alt = 'imagem do pokemon'
+      imagem.appendChild(img)
+  
+      const texto = document.createElement('div')
+      texto.className = 'carta_texto'
+      const nome = document.createElement('h1')
+      nome.className = 'nome'
+      const numero = document.createElement('p')
+      numero.className = 'numero'      
+      const tipo = document.createElement('p')
+      tipo.className = 'tipo'
+      const ataque = document.createElement('p')
+      ataque.className = 'ataque'
+      const vida = document.createElement('p')
+      vida.className = 'vida'     
+  
+      texto.appendChild(nome)
+      texto.appendChild(numero)      
+      texto.appendChild(tipo)      
+      texto.appendChild(ataque)
+      texto.appendChild(vida) 
+
+      carta.appendChild(imagem)
+      carta.appendChild(texto)
+
+      listaCartas.appendChild(carta)
+  
+      criaCarta(indice, carta)
+    }
 }
 
-criaCarta(numeroAleatorio())
-
-
-
+criaListaCartas()
